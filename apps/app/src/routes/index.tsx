b4a1904fa2a8dab7b87877d6b83de8b25cbde356
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-09-01 10:36:28 +0100

    Refactor routing and authentication components; add sign-up functionality with email and password handling; update database schema to include user, session, account, and verification tables; enhance routeTree for new routes and server integration.

--- apps/app/src/routes/index.tsx
+++ apps/app/src/routes/index.tsx
@@ -1,41 +1,9 @@
-import { createFileRoute, useRouter } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { db } from "@repo/database";
-import { testTable } from "@repo/database/schema";
+import { createFileRoute } from '@tanstack/react-router'
 
-async function readCount() {
-  return await db.select().from(testTable);
-}
+export const Route = createFileRoute('/')({
+  component: RouteComponent,
+})
 
-const getCount = createServerFn({
-  method: "GET",
-}).handler(() => {
-  return readCount().then((res) => res.length);
-});
-
-const updateCount = createServerFn({ method: "POST" }).handler(async () => {
-  await db.insert(testTable).values({});
-});
-
-export const Route = createFileRoute("/")({
-  component: Home,
-  loader: async () => await getCount(),
-});
-
-function Home() {
-  const router = useRouter();
-  const state = Route.useLoaderData();
-
-  return (
-    <button
-      type="button"
-      onClick={() => {
-        updateCount().then(() => {
-          router.invalidate();
-        });
-      }}
-    >
-      Add 1 to {state}?
-    </button>
-  );
+function RouteComponent() {
+  return <div>Hello "/"!</div>
 }
--- apps/app/src/routes/sign-up.tsx
+++ apps/app/src/routes/sign-up.tsx
@@ -1,9 +1,37 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { auth } from "@repo/auth/client";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import { useState } from "react";
 
 export const Route = createFileRoute("/sign-up")({
   component: RouteComponent,
 });
 
 function RouteComponent() {
-  return <div>Hello "/sign-up"!</div>;
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSignUp() {
+    auth.signUp.email({ email, name: email, password });
+    router.navigate({ to: "/" });
+  }
+
+  return (
+    <div>
+      <h1>Sign Up</h1>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <button onClick={handleSignUp} type="button">
+        Sign Up
+      </button>
+    </div>
+  );
 }
